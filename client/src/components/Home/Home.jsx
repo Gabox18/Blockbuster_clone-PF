89188d6100a42cb5b2689bc323466737d21adb34
silversurferbeats <gabrielsuarez89@gmail.com>
2022-09-12 11:57:25 -0300

    styles bootstrap and routing Landing & Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {allMovies} from '../../redux/actions';
+import "bootstrap/dist/css/bootstrap.min.css";
+import	'./Home.css';
+
 
 function Home(){
     let dispatch = useDispatch()
@@ -13,14 +16,22 @@ function Home(){
     return(
         <>
             {console.log(Movies)}
-            {Movies?.map((e,i)=>{
-                return (
-                <div key={i}>
-                    <h1>{e.Title}</h1>
-                    <img src={e.Poster} alt='img' height={'300px'}/>
-                </div>
-                )
-            })}
+            <div className="cardContainer">
+                {Movies?.map((e, i)=> {
+                    return (
+                        <>
+                            <div key={i} className="card" style={{width: '18rem'}}>
+                                <img src={e.Poster} className="card-img-top" alt="cardImg" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{e.Title}</h5>
+                                    <p className="card-text">{e.Plot}</p>
+                                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+                                </div>
+                            </div>                    
+                        </>
+                    )
+                })}
+            </div>
         </>
     )
 }
