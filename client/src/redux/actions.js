2e768ca407edc042cae6594d3f7f23ef8a8bd251
Santiago Hubert <santiagohubert11@gmail.com>
2022-09-13 15:50:41 -0300

    Detail-Render

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,6 +1,7 @@
 //import axios from "axios";
 import data from './dataMock.js'
 export const GET_ALL_MOVIES = 'GET_ALL_MOVIES'
+export const GET_DETAILS = 'GET_DETAILS'
 
   export const allMovies = () => {
     return async function(dispatch){
@@ -10,5 +11,16 @@ export const GET_ALL_MOVIES = 'GET_ALL_MOVIES'
           payload: data
           //payload: response.data
         })
+    }
+  }
+
+  export const getDetails = (id) => {
+    return async function(dispatch){
+      //let response = await axios.get(`http://localhost:3001/home/:${id}`)
+      return dispatch({
+        type: GET_DETAILS,
+        //payload: response.data
+        payload: data
+      })
     }
   }
