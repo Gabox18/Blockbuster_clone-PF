425d39ae3f006d08921635c17ee35c6cb101de35
silversurferbeats <gabrielsuarez89@gmail.com>
2022-09-15 18:33:52 -0300

    Detail style and ReactPlayer

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import './detail.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {asyncgetDetails,clearDetail} from '../../redux/slice.js';
+import ReactPlayer from 'react-player'
 
 
 export default function Detail() {
@@ -21,24 +22,48 @@ export default function Detail() {
       },[dispatch])
   
   return (
-      <div>
-      <h1>Title: {details[0]?.Title} ({details[0]?.id})</h1>
-          <h2>Actors: {details[0]?.Actors && details[0]?.Actors}</h2>
-          <h2>Year: {details[0]?.Year}</h2>
-          <h2>Genre: {details[0]?.Genre}</h2>
-          <h2>Director: {details[0]?.Director} </h2>
-          <h2>Language: {details[0]?.Language} km2</h2>
-          <h2>Ratings: {details[0]?.Ratings.length && details[0]?.Ratings.map((e,i) => {
-            return <p key={i}>{e.Value}</p>
-          })}</h2> 
-          <h2>Pais: {details[0]?.Country}</h2>
-          <div className="d-grid gap-2 mt-2 position-absolute">
-          <Link to ='/home'> 
-                  <button className="btn btn-primary btn-block mb-10 rounded-pill shadow-lg"
-                  type='shadow-lg p-3 mb-5 bg-body rounded'> Back </button>
-            </Link>
+    <>
+      <div className="card cardStyle col-auto mx-auto" >
+        <img src={details[0]?.Poster} className="card-img-top" alt="..." />
+        <div className='player-wrapper'>
+          <ReactPlayer
+            className='react-player'
+            url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            controls
+          />
         </div>
-
-    </div>
-  )
+        <div className="card-body">
+          <h2 className="card-title">Title: {details[0]?.Title} ({details[0]?.id})</h2>
+          <p className="card-text">Actors: {details[0]?.Actors && details[0]?.Actors}</p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Year: {details[0]?.Year}</li>
+          <li className="list-group-item">Genre: {details[0]?.Genre}</li>
+          <li className="list-group-item">Director: {details[0]?.Director}</li>
+          <li className="list-group-item">Language: {details[0]?.Language} km2</li>
+          <li className="list-group-item">
+            <h2>
+              Ratings:{" "}
+              {details[0]?.Ratings.length &&
+                details[0]?.Ratings.map((e, i) => {
+                  return <p key={i}>{e.Value}</p>;
+                })}
+            </h2>
+          </li>
+        </ul>
+        <div className="card-body">
+          <Link to="/home">
+            <button
+              className="btn btn-primary btn-block mb-10 rounded-pill shadow-lg"
+              type="shadow-lg p-3 mb-5 bg-body rounded"
+            >
+              {" "}
+              Back{" "}
+            </button>
+          </Link>
+          <a href="#" className="card-link">Another link</a>
+        </div>
+      </div>
+    </>
+  );
 }
