cb9de93bb74357c7593c81c912cc3528c8871d1d
silversurferbeats <gabrielsuarez89@gmail.com>
2022-09-16 16:14:05 -0300

    Navbar Style

--- client/src/components/Nav Bar/Navbar.jsx
+++ client/src/components/Nav Bar/Navbar.jsx
@@ -9,22 +9,22 @@ function Navbar(){
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className="navbar bg-dark navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand text-light" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
                             </li>
                             <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link text-light" href="#">Link</a>
                             </li>
                             <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a className="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </a>
                             <ul className="dropdown-menu">
@@ -35,12 +35,12 @@ function Navbar(){
                             </ul>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Shopping</a>
+                                <a className="nav-link disabled text-light">Shopping</a>
                             </li>
                         </ul>
                         <form className ="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <button className="btn btn-outline-success text-light" type="submit">Search</button>
                         </form>
                         <Logoutbutton/>
                         <Profile/>
