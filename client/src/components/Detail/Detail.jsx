7cd3f4a952e8ab987ae0068510397d652a1d7315
Gabriel Ferrer <gefv1891@gmail.com>
2022-09-16 10:33:03 -0400

    error in detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -4,38 +4,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import './detail.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {asyncgetDetails,clearDetail} from '../../redux/slice.js';
+import { asyncgetDetails, clearDetail } from '../../redux/slice.js';
 import ReactPlayer from 'react-player';
 
 
 
 export default function Detail() {
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    let {details} = useSelector(store => store.alldata);
-    
-    useEffect(() =>{
-        dispatch(asyncgetDetails(id))
-        console.log(details)
-        return ()=>{
-          dispatch(clearDetail())
-        }
-      },[dispatch])
-  
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  let { details } = useSelector(store => store.alldata);
+
+  useEffect(() => {
+    dispatch(asyncgetDetails(id))
+    console.log(details)
+    return () => {
+      dispatch(clearDetail())
+    }
+  }, [dispatch])
+
   return (
-      <div>
-      <h1>Title: {details[0]?.Title}</h1>
-          <h2>Actors: {details[0]?.Actors && details[0]?.Actors}</h2>
-          <h2>Year: {details[0]?.Year}</h2>
-          <h2>Genre: {details[0]?.Genre}</h2>
-          <h2>Director: {details[0]?.Director} </h2>
-          <h2>Language: {details[0]?.Language} km2</h2>
-          <h2>Ratings: {details[0]?.Ratings.length && details[0]?.Ratings.map((e,i) => {
-            return <p key={i}>{e.Value}</p>
-          })}</h2> 
-          <h2>Pais: {details[0]?.Country}</h2>
-    </div>
-  )
     <>
       <div className="card cardStyle col-auto mx-auto" >
         <img src={details[0]?.Poster} className="card-img-top" alt="..." />
@@ -47,7 +34,7 @@ export default function Detail() {
           />
         </div>
         <div className="card-body">
-          <h2 className="card-title">Title: {details[0]?.Title} ({details[0]?.id})</h2>
+          <h2 className="card-title">Title: {details[0]?.Title}</h2>
           <p className="card-text">Actors: {details[0]?.Actors && details[0]?.Actors}</p>
         </div>
         <ul className="list-group list-group-flush">
