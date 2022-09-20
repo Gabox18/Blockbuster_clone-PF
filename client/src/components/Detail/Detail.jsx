d43af9a9436005d89aec08396bbdcc18268b027f
Santiago Hubert <santiagohubert11@gmail.com>
2022-09-20 00:10:08 -0300

    Detail Style Responsive

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -6,6 +6,7 @@ import './detail.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { asyncgetDetails, clearDetail } from '../../redux/slice.js';
 import ReactPlayer from 'react-player';
+import video from '../../assets/video.mp4';
 
 
 
@@ -20,59 +21,64 @@ export default function Detail() {
     return () => {
       dispatch(clearDetail())
     }
-  }, [dispatch])
+  },[dispatch])
 
   return (
-    <>
     <div className="detailRender">
-      <div className="card cardStyle  mx-auto" >
-        <img src={details[0]?.Poster }  className="card-img-top" alt="..." />
-        <div className='player-wrapper'>
+      <div className="videoBg">
+        <video src={video} muted loop autoPlay></video>
+      </div>
+      <div className="cardStyle">
+        <img src={details[0]?.Poster} className="card-img-top" alt="..." />
+        <div className="player-wrapper">
           <ReactPlayer
-            className='react-player'
-            url='https://youtu.be/Oy_SER6dfK4'
+            className="react-player"
+            url="https://youtu.be/Oy_SER6dfK4"
             controls
           />
-        </div> 
-        <br></br><h2 className="card-title"> {details[0]?.Title}</h2> 
-         <p className="card-text">Actors: {details[0]?.Actors && details[0]?.Actors}</p>
-        <div className="column-detail">
-        <ul >
-          <div className='column-left'>
-          <li className="list-group-item"><p>Year: <b> {details[0]?.Year}</b></p></li>
-          <li className="list-group-item"><p>Genre: <b>{details[0]?.Genre}</b></p></li>
-          <div>
-          <li className="list-group-item"><p>Director: <b>{details[0]?.Director}</b></p></li>
-          <li className="list-group-item"><p>Language: <b>{details[0]?.Language}</b></p></li>
-          </div>
-          </div>
-          <div className='column-right'>
-          <li className="list-group-item">
-            <h2>
-              Ratings:{" "}
-              {details[0]?.Ratings.length &&
-                details[0]?.Ratings.map((e, i) => {
-                  return <p key={i}>{e.Value}</p>;
-                })}
-            </h2>
-          </li>
-          </div>
-        </ul>
-        </div>
-        <div className="card-body">
-          <Link to="/home">
-            <button
-              className="btn btn-primary btn-block mb-10 rounded-pill shadow-lg"
-              type="shadow-lg p-3 mb-5 bg-body rounded"
-            >
-              {" "}
-              Back{" "}
-            </button>
-          </Link>
-          {/* <a href="#" className="card-link">Another link</a> */}
         </div>
       </div>
+      <div className="cardDetail">
+        <p>
+          <span>{details[0]?.Title}</span>
+        </p>
+        <p>
+          <span>Actors: {details[0]?.Actors && details[0]?.Actors}</span>
+        </p>
+        <p>
+          <span>Year: {details[0]?.Year}</span>
+        </p>
+        <p>
+          <span>Genre: {details[0]?.Genre}</span>
+        </p>
+        <p>
+          <span>Director: {details[0]?.Director}</span>
+        </p>
+        <p>
+          <span>Language: {details[0]?.Language}</span>
+        </p>
+        <p>
+          <span>
+            Ratings:{" "}
+            {details[0]?.Ratings.length &&
+              details[0]?.Ratings.map((e, i) => {
+                return <p key={i}>{e.Value}</p>;
+              })}
+          </span>
+        </p>
       </div>
-    </>
+      <div className="card-body col-auto p-5 justify-content-center">
+        <Link to="/home">
+          <button
+            className="btn btn-primary btn-block mb-10 rounded-pill shadow-lg"
+            type="shadow-lg p-3 mb-5 bg-body rounded"
+          >
+            {" "}
+            Back{" "}
+          </button>
+        </Link>
+        {/* <a href="#" className="card-link">Another link</a> */}
+      </div>
+    </div>
   );
 }
