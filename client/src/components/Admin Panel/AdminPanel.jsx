f6b3aac9faecd7e50dc6b41531677d4a0d54293c
Gabriel Ferrer <gefv1891@gmail.com>
2022-09-24 17:37:24 -0400

    error in formulario admin

--- client/src/components/Admin Panel/AdminPanel.jsx
+++ client/src/components/Admin Panel/AdminPanel.jsx
@@ -1,9 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 import axios from "axios";
-import { useState} from "react";
+import { Link} from "react-router-dom";
+import "./adminPanel.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 import { useDispatch} from "react-redux";
-import { Link, useHistory } from "react-router-dom";
-
 function validate(input) {
   let errors = {};
   if (!input.name || input.name.length < 3) {
@@ -19,10 +20,8 @@ function validate(input) {
   }
   return errors;
 }
-
 export default function AdminPanel() {
   const dispatch = useDispatch();
-  const history = useHistory();
   const [errors, setErrors] = useState({})
   const [imagen, setImagen] = useState("");
   const [input, setInput] = useState({
@@ -32,27 +31,22 @@ export default function AdminPanel() {
     recommendation: "",
     image: "",
   });
-
   const uploadImage = async (e) => {
     const files = e.target.files;
     const data = new FormData();
     data.append("file", files[0]);
     data.append("upload_preset", "BlockBuster");
-
     const respuesta = await axios.post(
       "https://api.cloudinary.com/v1_1/dapicfoap/image/upload",
       data
     );
-
     console.log(respuesta);
-
     setImagen(respuesta.data.secure_url); //esa url se va a la DB
     setInput({
       ...input,
       image: respuesta.data.secure_url,
     });
   };
-
     function handleChange(e) {                      //cada vez que se ejecuta handlechange, al estado input,
       setInput({                                  //ademas de lo que tiene, se le agrega el target.value
           ...input,
@@ -64,7 +58,6 @@ export default function AdminPanel() {
       }));
       console.log(input);
   }
-
   function handleSubmit(e) {      
     console.log(errors)          
     e.preventDefault();
@@ -82,85 +75,76 @@ export default function AdminPanel() {
       recommendation: "",
       image: "",
     })
-    history.push('/home');
   }
   }
-
   return (
-    <div className="admin-containPanel">
+    <div className="admin-contain">
       <div>
         <h1 className="admin-titulo"> ¡Share your movie and let's expand the cinema at home! </h1>
       </div>
       {/* //onSubmit={(e)=> handleSubmit(e)} */}
-      <form className='formularioAdmin'> 
+      <form className='formulario' onSubmit={(e)=>handleSubmit(e)}>
         <div>
-          <div className="mb-3">
-            <label htmlForcl=""> Name of the movie: </label>
-            <input
-            placeholder= "example, Titanic"
-            type="text"
-            value={input.name}
+          <label htmlForcl=""> Name of the movie: </label>
+
+          <input
+            placeholder="example, Titanic"
+            type="text" value={input.name}
             name="name"
             autocomplete="off"
-            />
-          </div>
-          <div className="mb-3">
+            onChange={(e)=>handleChange(e)}
+          />
+          <div>
             <label className=""> Description: </label>
-            <input
-            placeholder="example based on, tells the story..."
-            type="text"
-            value={input.name}
-            name="description"
-            autocomplete="off"
-              />
+            <input placeholder="example, based on, tells the story..."
+              type="text"
+              value={input.description}
+              name="description"
+              autocomplete="off"
+              onChange={(e)=>handleChange(e)}
+            />
           </div>
           <div>
             <label className=""> Genre: </label>
-          <input
-          placeholder= "ej. Action, Comedy"
-          type="text"
-          value={input.name}
-          name="genre"
-          autocomplete="off"
+            <input placeholder="example, Action, Comedy"
+              type="text"
+              value={input.genre}
+              name="genre"
+              autocomplete="off"
+              onChange={(e)=>handleChange(e)}
             />
           </div>
-          <div>  
+          <div>
             <label className='display-block'> Recommendation </label>
-          <select
+            <select
               name="recommendation"
               value={input.recommendation}
               className=""
               onChange={(e) => handleChange(e)}>
               <option value=""> Choose your score </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              {errors.recommendation && <p className="error">{errors.recommendation}</p>} 
+              <option value="1">⭐</option>
+              <option value="2">⭐⭐</option>
+              <option value="3">⭐⭐⭐</option>
+              <option value="4">⭐⭐⭐⭐</option>
+              <option value="5">⭐⭐⭐⭐⭐</option>
+              {errors.recommendation && <p className="error">{errors.recommendation}</p>}
             </select>
           </div>
-    
+
           <input
             name="image" //ESTE INPUT NO PUEDE TENER VALUE
             placeholder="Carga aquí tu img"
             type="file"
             onChange={uploadImage}
           />
-
           <div>
             <img src={imagen} alt="" />
           </div>
-
-          <button className="submit-buttonAdmin" type="submit"> Load movie </button>
+          <button className="submit-button" type="submit"> Load movie </button>
         </div>
       </form>
       <Link to='/home'>
-        <button 
-        className="btn btn-outline-warning btn-block mb-10 rounded-pill shadow-lg"
-        type="shadow-lg p-3 mb-5 bg-body rounded">
-
-        Back </button>
+        <button className="btn btn-outline-warning btn-block mb-10 rounded-pill shadow-lg" type="shadow-lg p-3 mb-5 bg-body rounded"> Back </button>
       </Link>
     </div>
   );
