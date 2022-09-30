e335f4ca20fcbe9e6cb18e9bbf555294b13990fc
Franco Selvarolo <fselvarolo28@gmail.com>
2022-09-30 14:17:56 -0300

    algunas cosas

--- client/src/components/Nav Bar/Navbar.jsx
+++ client/src/components/Nav Bar/Navbar.jsx
@@ -5,31 +5,24 @@ import FilteringSorting from "./filtering&sorting/filtering&sorting.jsx";
 import Searchbar from "./search bar/search.jsx";
 import img from "../../assets/Logo.png";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link, Route} from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import { asyncGetUser } from "../../redux/slice.js";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
-
 function Navbar(prop) {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
-    let userDB = useSelector(state=>state.alldata.user)
-    let dispatch = useDispatch()
-    console.log(typeof(userDB)==='string')// verifica si lo que hay en store.user === 'string
-    useEffect (()=>{
-        dispatch(asyncGetUser(user?.email))
-    },[dispatch, user?.email])
+  let userDB = useSelector((state) => state.alldata.user);
+  let dispatch = useDispatch();
+  console.log(typeof userDB === "string"); // verifica si lo que hay en store.user === 'string
+  useEffect(() => {
+    dispatch(asyncGetUser(user?.email));
+  }, [dispatch, user?.email]);
 
   return (
     <>
-      <nav className="navbarFix navbar bg-dark navbar-expand-lg bg-light">
+      <nav className="navbar-expand-lg">
         <div className="container-fluid">
-          <div className="navbar-brand text-light">
-          
-            <Link to={"/home"}>
-              <img src={img} width="60px" alt="logo" />
-            </Link>
-          </div>
           <button
             className="navbar-toggler navbarButon"
             type="button"
@@ -42,56 +35,89 @@ function Navbar(prop) {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <Route  path="/home"> 
+            <Route path="/home">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <Route path="/home">
+                  <FilteringSorting setCurrentPage={prop.setCurrentPage} />
+                </Route>
+              </ul>
 
-                {/*para que no se rendererise los filtrados en el componete de profile */}
-
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <Route  path='/home'>
-              <FilteringSorting setCurrentPage={prop.setCurrentPage} />
-                        </Route>
-            </ul>
-                        
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0"> 
-                        <Route  path='Home/result'>
-                            <FilteringSorting setCurrentPage={prop.setCurrentPage}/> 
-                        </Route>
-                        </ul>
-                       
-                 {/*para que no se rendererise los filtrados en el componete de profile */}
-
-          </Route>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <Route path="Home/result">
+                  <FilteringSorting setCurrentPage={prop.setCurrentPage} />
+                </Route>
+              </ul>
+              <ul className="logoNav"><Link to={"/home"}>
+              <img src={img} width="90px" alt="logo" />
+            </Link></ul>
+              {/*para que no se rendererise los filtrados en el componete de profile */}
+            </Route>
+            
             <Route path="/home">
               <Searchbar setCurrentPage={prop.setCurrentPage} />
             </Route>
-            <Route strict path="/home/">
+            <Route path="/details/:id">
+              <ul className="logoNavDetail"><Link to={"/home"}>
+              <img src={img} width="90px" alt="logo" />
+            </Link></ul>
+              <Searchbar className="searchDetail" setCurrentPage={prop.setCurrentPage} />
+              {isAuthenticated ? (
+                <>
+                  <Logoutbutton />
+                  <Link to={"/profile"}>
+                    <img
+                      src={
+                        typeof userDB === "string"
+                          ? user.picture
+                          : userDB.picture
+                      }
+                      alt="profile"
+                      width={"40px"}
+                      className="imgPerfil"
+                    />
+                  </Link>
+                </>
+              ) : (
+                <button
+                  type="buttonNavbar"
+                  className="btn btn-outline-primary text-light btn-xs btnLogin"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Login
+                </button>
+              )}
+            </Route>
             
-          </Route >
-          <Route path="/home">
-            {isAuthenticated ? (
-              <>
-                <Logoutbutton />
-                <Link to={"/profile"}>
-                  <img
-                    src={typeof(userDB)==='string'?user.picture:userDB.picture}
-                    alt="profile"
-                    width={"40px"}
-                    className="imgPerfil"
-                  />
-                </Link>
-              </>
-            ) : (
-              <button
-                type="buttonNavbar"
-                className="btn btn-outline-primary text-light btn-xs btnLogin"
-                onClick={() => loginWithRedirect()}
-              >
-                Login
-              </button>
-            )}
-            </Route >
+            <Route strict path="/home/"></Route>
+            <Route path="/home">
+              {isAuthenticated ? (
+                <>
+                  <Logoutbutton />
+                  <Link to={"/profile"}>
+                    <img
+                      src={
+                        typeof userDB === "string"
+                          ? user.picture
+                          : userDB.picture
+                      }
+                      alt="profile"
+                      width={"40px"}
+                      className="imgPerfil"
+                    />
+                  </Link>
+                </>
+              ) : (
+                <button
+                  type="buttonNavbar"
+                  className="btn btn-outline-primary text-light btn-xs btnLogin"
+                  onClick={() => loginWithRedirect()}
+                >
+                  Login
+                </button>
+              )}
+            </Route>
             <Route exact path="/">
-            <button
+              <button
                 type="buttonNavbar"
                 className="btn btn-outline-primary text-light btn-xs btnLogin"
                 onClick={() => loginWithRedirect()}
