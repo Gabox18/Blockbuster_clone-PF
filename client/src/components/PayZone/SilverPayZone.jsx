dcab9eead984eaf10ca384a4c6ff96f66d49efb9
Gabriel Ferrer <gefv1891@gmail.com>
2022-10-05 15:42:58 -0400

    prueba pasarela 1 solo silver

--- client/src/components/PayZone/SilverPayZone.jsx
+++ client/src/components/PayZone/SilverPayZone.jsx
@@ -3,24 +3,26 @@ import Navbar from '../Nav Bar/Navbar'
 import { asyncCategorySwich } from '../../redux/slice'
 import './SilverPayZone.css'
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 export default function SilverPayZone(){
     let dispatch = useDispatch();
     let userDB = useSelector((state) => state.alldata.user);
-    const { token } = useParams();
-    let location = useLocation();
-
+    
 
     function handleSubmitSilver() {
-        
-        console.log(token,'soy el token')
-        dispatch(asyncCategorySwich(userDB.id))
+        let idSwich = {id : userDB.id}
+        dispatch(asyncCategorySwich(idSwich))
+
       }
-      console.log(window.location.href,"url-------------->con window.location.href")
-      console.log(location.pathname,"url-------------->con pathname")
+      console.log(window.location.href)
+      let url = window.location.href
+      let tokenredirec = url.split('').slice(47,64).join('')
+      console.log(tokenredirec)
     return(
-        <>
+        tokenredirec === userDB.token?
+        <div>
             <div>
                 <Navbar/>
             </div>
@@ -38,6 +40,7 @@ export default function SilverPayZone(){
                         </div>
                     </div>
                 </div>
+                <Link to={'/home'}>
                 <button onClick={handleSubmitSilver}>
                     <div class="default-btn">
                     <svg class="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none" stroke-width="2" stroke="#ffd300" height="20" width="20" viewBox="0 0 24 24"><circle r="1" cy="21" cx="9"></circle><circle r="1" cy="21" cx="20"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
@@ -48,7 +51,9 @@ export default function SilverPayZone(){
                         <span>continue</span>
                     </div>
                 </button>
+                </Link>
             </section>
-        </>
+        </div>:
+        <div><Link to={"/home"}>try again </Link></div>
     )
 }
