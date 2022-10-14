5304042c2ce405c8f2474c82281bd5ced8829c32
Santiago Hubert <santiagohubert11@gmail.com>
2022-10-14 13:02:19 -0300

    FooterRespon

--- client/src/components/Footer/Legal.jsx
+++ client/src/components/Footer/Legal.jsx
@@ -5,27 +5,30 @@ import "./Legal.css";
 
 export default function Legal() {
   return (
-    <div className="all-legal">
-      <Navbar/>
-      <div className="legal-information">
-        <div className="noseque">
-          <h1 className="">
-            <b className="legal-title">Legal Information</b>
-          </h1>
-          <p className="p-legal">
-            Blockbuster is a project made by students from{" "}
-            <a href="https://www.soyhenry.com/">Henry</a> as part of an
-            assignment.
-          </p>
-          <p className="p-legal">It is not a real nor functional website, just a made-up one.</p>
-          <p className="p-legal">None of the products displayed can be sold or purchased.</p>
-          <p className="p-legal">
-            All the product images, descriptions and serial numbers used are for
-            demostrative purposes only.
-          </p>
+    <>
+      <Navbar />
+      <div className="all-legal">
+        <div className="legal-information">
+          <div className="legal">
+            <h1>
+              <b className="legal-title">Legal Information</b>
+            </h1>
+            <p>
+              Blockbuster is a project made by students from{" "}
+              <a href="https://www.soyhenry.com/">Henry</a> as part of an
+              assignment.
+            </p>
+            <p>It is not a real nor functional website, just a made-up one.</p>
+            <p>None of the products displayed can be sold or purchased.</p>
+            <p>
+              All the product images, descriptions and serial numbers used are for
+              demostrative purposes only.
+            </p>
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
+
   );
 }
